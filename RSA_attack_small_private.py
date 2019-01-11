f8e9bae3f752f8369f03ac7c56cee2d111ddb845
Melanie <melanie.alwardt@gmail.com>
2019-01-11 12:41:53 +0100

    should work, but is not.... need to check input

--- RSA_attack_small_private.py
+++ RSA_attack_small_private.py
@@ -40,8 +40,8 @@ def factorize(e, n):
             # calculate residue
             r[0] = (Fraction(str(e) + '/' + str(n)) - int(qi[i]))
         else:
-            if i == 381:
-                print("hello")
+            #if i == 381:
+                #print("hello")
             # round down 1/residue_from_last_iteration
             qi.append(int(1/r[0]))
             # copy list for usage
@@ -93,6 +93,5 @@ if __name__ == "__main__":
     #homework variables
     n1 = 0x5ffa391905f026a9e64eec553e37209938977ac11d1ef969d3848a9e2f77dd33fc12fb698b872ba1bd3bf5ac6e02a1fd24e6c26f95790f7a21e3f51a806919cd7254a7bb1ec3a23d7c0f7e773ffce083f09daaf34ef84b25df14c8114ce4b4fbf7b20646ea23aef46b51e62f395c966f20c906a1dea3a909e9d4093503ebd833
     e1 = 0x2025b7ef3c7484a7ed9da144c3b92159fad4a7bd4159fd16b83256d92814fb09ee8f13606ed4982afa69e518511e9b0f0fc3d9220dca82a416a4ec9891adf48b242a2e3e2dd5fc0f685aff1817c29a47ee83977a3253af1a0d6dcc3ae1806d0dc8f9c247e19bd5b8305ebad0c043b356c7d81d9e9f927e2f833ccf862b521063
-
     #factorize(2621, 8927)
     factorize(n1, e1)
